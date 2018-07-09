21739b0a930fb6c6bf08c2575554d50c793b01ec
Lucas Leschynski <mr.lukcio@gmail.com>
2018-07-08 21:11:43 -0500

    Finished fireworks

--- sketch.js
+++ sketch.js
@@ -6,50 +6,21 @@ function setup() {
     //firework = new Particle(random(0,width),height)
     gravity = new p5.Vector(0, 0.18)
     noStroke()
+    background(45)
 }
 
 function draw() {
-    background(51)
-    if(random(1) < 0.05) {
-        fireworks.push(new Particle(random(width), height))
+    colorMode(RGB)
+    background(0, 50)
+    if(random(1) < 0.02) {
+        fireworks.push(new Firework())
     }
-    for(var i = 0; i < fireworks.length; i++) {
+    for(var i = this.fireworks.length-1; i >= 0; i--) {
         fireworks[i].update()
         fireworks[i].show()
-        fireworks[i].applyForce(gravity)
-    }
-}
-
-function Particle(x, y) {
-    this.pos = new p5.Vector(x,y)
-    this.vel = new p5.Vector(0,-10)
-    this.acc = new p5.Vector(0,0)
-
-    this.applyForce = function(force) {
-        this.acc.add(force)
-
-    }
-
-    this.update = function() {
-        this.vel.add(this.acc)
-        this.pos.add(this.vel)
-        this.acc.mult(0)
-    }
-
-    this.show = function() {
-        fill('white')
-        ellipse(this.pos.x,this.pos.y,2,2)
-    }
-}
-function Firework() {
-    this.firework = new Particle(random(width), height)
-
-    this.update = function() {
-        this.firework = applyForce(gravity)
-        this.firework = update()
-    }
-
-    this.show = function() {
-        this.firework.show()
+        if(this.fireworks[i].done()) {
+            this.fireworks.splice(i, 1)
+        }
+        //fireworks[i].applyForce(gravity)
     }
 }
